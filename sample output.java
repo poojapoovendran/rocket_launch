0cbdc9c945232d06d2acb8cea1913ce96a087999
poojapoovendran <146953710+poojapoovendran@users.noreply.github.com>
2023-10-05 10:04:24 +0530

    Add files via upload

---
+++ sample output.java
@@ -0,0 +1,26 @@
+Welcome to the Rocket Launch Simulator!
+Initial State: Stage: Pre-launch, Fuel: 100%, Altitude: 0 km, Speed: 0 km/h
+User Input:
+1. Pre-Launch Checks: Type 'start_checks' to initiate system checks.
+2. Launch: Type 'launch' to begin the mission after checks are complete.
+3. Fast Forward: Type 'fast_forward X' to advance the simulation by X seconds.
+Enter the command: 
+
+start_checks
+All systems are 'Go' for launch.
+
+launch
+Rocket launched!
+Stage: 0, Fuel: 90%, Altitude: 0 km, Speed: 10 km/h
+Stage: 0, Fuel: 80%, Altitude: 20 km, Speed: 30 km/h
+Stage: 0, Fuel: 70%, Altitude: 50 km, Speed: 60 km/h
+Stage: 0, Fuel: 60%, Altitude: 90 km, Speed: 100 km/h
+Stage: 1, Fuel: 50%, Altitude: 140 km, Speed: 150 km/h
+Stage 1 complete. Separating stage. Entering Stage 2.
+Stage: 2, Fuel: 40%, Altitude: 190 km, Speed: 200 km/h
+Stage: 2, Fuel: 30%, Altitude: 240 km, Speed: 250 km/h
+...
+(Continues updating every second)
+
+fast_forward 30
+Fast forward 30 seconds: Orbit achieved! Mission Successful
